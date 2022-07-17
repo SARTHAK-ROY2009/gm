845f5cbe583b899e164bed557fb3a003f559ab3c
SARTHAK-ROY2009 <97378801+SARTHAK-ROY2009@users.noreply.github.com>
2022-07-17 15:38:40 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+// Create login Function here.
+function Login() {
+    player_name=document.getElementById("playername").value;
+    localStorage.setItem("playername", playername);
+    window.location="gamepage.html";
+}
